bcd8d3e29c9bf192dba96a86fc6b5bf6d6ca4108
Datla Madhavi Rohitha <79572509+Madhavirohithadatla@users.noreply.github.com>
2021-08-26 10:40:29 +0530

    Add files via upload

---
+++ secondlargest.py
@@ -0,0 +1,19 @@
+
+list1 = []
+num = int(input("Enter number of elements in list: "))
+ 
+for i in range(1, num + 1):
+    ele = int(input("Enter elements: "))
+    list1.append(ele)
+ 
+'''
+# sort the list   
+list1.sort()
+     
+# print second maximum element
+print("Second largest element is:", list1[-2])
+ 
+'''
+ 
+# print second maximum element using sorted() method
+print("Second largest element is:", sorted(list1)[-2])
